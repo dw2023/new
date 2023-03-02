2e1ddfcedb4d006b934e92df8a62f6828d363144
dw2023 <dwkim126@gmail.com>
2023-03-02 21:42:56 +0900

    1단계

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc; // final: 딱 한 번만 값을 대입할 수 있음 (수정불가)
+
+    // 생성자
+    // Main 클래스가 만든 Scanner를 App 클래스도 쓸 수 있도록 객체를 생성자를 통해 넘겨줬다.
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        // '종료'가 입력될 때는 제외하고 계속 반복해야 함 -> while
+        // 그렇지 않으면 enter만 쳐도 종료됨
+       while (true) {
+           System.out.print("명령) ");
+           String command = sc.nextLine().trim();
+           // .trim() : (혹시 있을지도 모를) 문자열의 좌우공백을 없앤 새로운 문자열을 리턴
+           // "  종료 "가 입력되도 종료시키기 위해서
+
+           if ( command.equals("종료")) {
+               break;
+           }
+       }
+    }
+}
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        new App(sc).run();
+        // run()을 실제로 구현하기 전에 ctrl + 1하면, run()이 클래스 App에 자동생성됨
+        // Main 클래스가 만든 Scanner를 App 클래스도 쓸 수 있도록 객체를 생성자를 통해 넘겨줬다.
+
+        sc.close();
     }
 }
