ac27b82f4cceca522a7fbae9b5cd1897d7ce1da9
dw2023 <dwkim126@gmail.com>
2023-03-04 16:35:45 +0900

    5단계

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -13,7 +15,8 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        long lastWiseSayingId = 1; // while문 안에 넣으면 계속 1로 초기화됨
+        long lastWiseSayingId = 0; // while문 안에 넣으면 계속 0으로 초기화됨
+        List<WiseSaying> wiseSayings = new ArrayList<>(); // 없어지지 않게 while문 밖에 만들기
 
         // '종료'가 입력될 때는 제외하고 계속 반복해야 함 -> while
         // 그렇지 않으면 enter만 쳐도 종료됨
@@ -26,13 +29,29 @@ public class App {
            if ( command.equals("종료")) {
                break;
            } else if (command.equals("등록")) {
+               long id = lastWiseSayingId + 1;
                System.out.print("명언 : ");
                String content = sc.nextLine().trim();
                System.out.print("작가 : ");
                String authorName = sc.nextLine().trim();
 
-               System.out.printf("%d번 명언이 등록되었습니다.\n", lastWiseSayingId);
-               lastWiseSayingId++; // 증가
+               // WiseSaying 객체 만들기
+               WiseSaying wiseSaying = new WiseSaying(id, content, authorName);
+
+               // WiseSaying 객체를 List나 배열에 저장하지 않으면 이 중괄호가 끝나고 사라짐.
+               // 명언을 무한히 입력할 수 있게 WiseSaying 객체를 List에 저장
+               wiseSayings.add(wiseSaying);
+
+               System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+               lastWiseSayingId = id; // lastWiseSayingId를 1 증가
+           } else if (command.equals("목록")) {
+               System.out.println("번호 / 작가 / 명언");
+               System.out.println("-".repeat(22));
+
+               for (int i = wiseSayings.size() - 1; i >= 0 ; i--) {
+                   WiseSaying wiseSaying = wiseSayings.get(i);
+                   System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthorName(), wiseSaying.getContent());
+               }
            }
        }
     }
---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class WiseSaying {
+    private long id;
+    private String content;
+    private String authorName;
+
+    // alt + insert -> constructor : 생성자 만들기
+    public WiseSaying(long id, String content, String authorName) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+
+    // alt + insert -> Getter 메서드 만들기
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+}
